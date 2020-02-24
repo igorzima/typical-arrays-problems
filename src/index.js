4c5bc1327e39516e59229bb9b9e1905905fa31c1
igorzima <redteam122@gmail.com>
2020-02-24 18:06:27 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array == "") {
+        return 0;
+    }
+
+    let min = array.sort(function(a, b) {
+        return a - b;
+    });
+
+    return min[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array == "") {
+        return 0;
+    }
+
+    let max = array.sort(function(a, b) {
+        return a - b;
+    });
+    
+    return max[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array == "") {
+        return 0;
+    }
+
+    let sum = array.reduce(function(a, b) {
+        return a + b;
+    })
+    
+    return sum / array.length;
 }
